1c972d3949b8af7d708896fbdd3b67bbdbe1ad67
Stuy-Kevin-Lin <klin14@stuy.edu>
2019-05-08 22:30:19 -0400

    Server side countdown and real time drawing proofs of concept

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
 from flask_socketio import SocketIO, join_room, leave_room, emit, send
 from flask import Flask, render_template, request
+import threading
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
 socketio = SocketIO(app)
 
+continueTimer = True
+timerTime = 60 #Timer to be displayed
 rooms = {}
+currLines = []
+
+@app.before_first_request #Executed upon startup
+def setup():
+    countdown() #Start countdown timer
 
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("index.html", currTime = timerTime)
+
+@socketio.on('requestLines')
+def returnLines(data):
+    emit('recieveLines', currLines)
+
+@socketio.on('clearBoard')
+def clearBoard(data):
+    currLines = []
+    # print(currLines)
+    emit('clearBoard', None, broadcast = True, include_self = False)
+
+@socketio.on('newLine')
+def newLine(line):
+    currLines.append(line);
+    # print(line);
+    emit('newLine', line, broadcast = True, include_self = False)
+
+def countdown():
+    global continueTimer, timerTime
+    if continueTimer:
+        threading.Timer(1, countdown).start()
+        #Execute the following tasks every second
+        timerTime -= 1
+        if timerTime <= -1:
+            timerTime = 60
+        socketio.emit('updateTimer', timerTime)
 
 #@socketio.on("eventName")
 #def fxn(data):
--- static/socketstuff.js
+++ static/socketstuff.js
@@ -1,6 +1,79 @@
 var socket = io.connect('http://' + document.domain + ":" + location.port);
+var timer = document.getElementById('timer');
+var canvas = document.getElementById('drawingCanvas');
+var ctx = canvas.getContext('2d');
+var clearbtn = document.getElementById('clearbtn');
+var isDrawing = false;
+var prevX = 0;
+var prevY = 0;
+var lineWidth = 3;
+
+ctx.lineCap = 'round';
+
+var drawLine = function(x0, y0, x1, y1, sendBack = true) {
+  ctx.lineWidth = lineWidth;
+  ctx.beginPath();
+  ctx.moveTo(x0, y0); //Offset x and y by vector
+  ctx.lineTo(x1, y1); //Draw line to center of the next circle
+  ctx.stroke();
+  if (sendBack) {
+    socket.emit('newLine', [x0, y0, x1, y1]);
+  }
+}
+
+var clearBoard = function(sendBack = true) {
+  ctx.clearRect(0,0,canvas.width,canvas.height); //Clears the entire canvas
+  if (sendBack) {
+    socket.emit('clearBoard', null);
+  }
+}
 
 socket.on('connect', function() { //Executed upon opening the site
   console.log('Successfully Connected');
   socket.emit('joinRoom', 'General'); //Automatically join General
-})
+  socket.emit('requestLines', null);
+});
+
+socket.on('recieveLines', function(lines) {
+  for (var i = 0; i < lines.length; i += 1) {
+    currLine = lines[i];
+    drawLine(currLine[0], currLine[1], currLine[2], currLine[3], sendBack = false);
+  }
+});
+
+socket.on('newLine', function(line) {
+  // console.log(line);
+  drawLine(line[0], line[1], line[2], line[3], sendBack = false);
+});
+
+socket.on('clearBoard', function(data) {
+  clearBoard(sendBack = false);
+});
+
+socket.on('updateTimer', function(newTime) {
+  timer.innerHTML = newTime;
+});
+
+canvas.addEventListener('mousedown', function(e) {
+  prevX = e.offsetX;
+  prevY = e.offsetY;
+  isDrawing = true;
+  drawLine(prevX, prevY, prevX, prevY);
+});
+
+canvas.addEventListener('mousemove', function(e) {
+  if (isDrawing) {
+    //Draw the line
+    drawLine(prevX, prevY, e.offsetX, e.offsetY);
+    prevX = e.offsetX;
+    prevY = e.offsetY;
+  }
+});
+
+canvas.addEventListener('mouseout', function(e) {
+  isDrawing = false;
+});
+
+canvas.addEventListener('mouseup', function(e) {
+  isDrawing = false;
+});
