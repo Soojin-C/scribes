ad7913e7b1b40f52563ee9c818156eb9e9d58f94
Angela <atom@stuy.edu>
2019-06-07 05:00:20 -0400

    i need sleep

--- scribble/util/db_user.py
+++ scribble/util/db_user.py
@@ -29,7 +29,7 @@ def build():
 def spic(username):
     db=sqlite3.connect(DB_FILE)
     c=db.cursor()
-    command="SELECT pic FROM profile WHERE username=?"
+    command="SELECT pic FROM profile WHERE user=?"
     c.execute(command,(username,))
     output = c.fetchone()
     print(output)
